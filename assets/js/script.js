03d99f7dda931118a999759ae7f52096d39ea096
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-09-22 10:08:10 -0400

    added more game functionality to complete game and calculate score

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ var startButton = document.querySelector(".start-button");
 var questionContainerEl = document.querySelector(".question");
 var answerContainerEl = document.querySelector("#answer-buttons");
 var headerEl = document.querySelector(".headers");
-var answeredButtons = document.querySelector(".answer-btn");
-var currentQuestion;
-console.log(answeredButtons);
+var answeredButtons = document.querySelectorAll(".answer-btn");
+var answerDisplayEl = document.querySelector(".displayAnswer");
+var endGameEl = document.querySelector(".results");
+var finalScore = document.querySelector("#finalScore");
+var score = 0;
+var submitButton = document.querySelector("#submitBtn");
+var scoreLinkEl = document.querySelector("#scoreLink");
 
 // create array of questions for quiz
 const questions = [
@@ -35,15 +39,40 @@ const questions = [
             {text: "div", correct: false},
             {text: "id", correct: true}
         ]
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            {text: "Color and Style Sheets", correct: false},
+            {text: "Cascading Style Sheets", correct: true},
+            {text: "Cascade Sheets Style", correct: false},
+            {text: "Coded Style Sheet", correct: false}
+        ]
+    },
+    {
+        question: "Which statement cannot be used to declare a variable in JavaScript?",
+        answers: [
+            {text: "Let", correct: true},
+            {text: "Var", correct: false},
+            {text: "Const", correct: false},
+            {text: "Int", correct: false}
+        ]
     }
 ]
 
+// event listener to initialize game upon button click
 startButton.addEventListener("click", startGame);
 
 // determine if the selected button answer is correct with event listener
-for (const answerBtn of answeredButtons) {
+
+for (const answerBtn of Array.from(answeredButtons)) {
     answerBtn.addEventListener("click", isCorrect);
+    
 }
+
+// event handler for when user submits their score and initials to score board
+submitButton.addEventListener("click", makeScore);
+
 
 // create function to initialize the game by hiding start button and instructions and showing first question
 function startGame() {
@@ -67,9 +96,34 @@ function setQuestion() {
     
 }
 
-// determine if selected button is correct answer
+// determine if selected button is correct answer and display it
 function isCorrect() {
-    console.log(this.dataset.correct);
+    answerDisplayEl.classList.remove("hide");
+    if(this.dataset.correct == "true") {
+        answerDisplayEl.textContent = "Correct!"
+        score += 5;
+    } else {
+        answerDisplayEl.textContent = "Wrong!"
+    }
+    
+    if(questions.length > 0) {
+        setQuestion();
+    } else {
+        endGame();
+    }
+}
 
+//function to handle finishing the game and calculating score
+function endGame() {
+    answerDisplayEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    answerContainerEl.classList.add("hide"); 
+    endGameEl.classList.remove("hide");
+
+    finalScore.textContent = score;
+
+}
+
+function makeScore() {
     
 }
